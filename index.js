8331509456e3893dcf8b2224dfd69a325e9ea11c
0374flop <mikola.golovach10@gmail.com>
2025-10-25 16:41:29 +0300

    реадми

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const lib = require('./testserverlist');
 module.exports = lib;
+
+if (require.main === module) (async () =>{
+    console.log(JSON.stringify(await lib.getrawDDNetServers(), null, 2))
+})();
